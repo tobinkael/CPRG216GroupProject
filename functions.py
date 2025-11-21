548e1c65ed7e25259ae3df7085c5b8f94d41552e
DerpyDerrick <derickhesait@gmail.com>
2025-11-20 17:34:39 -0700

    added assignment 2 dir

---
+++ functions.py
@@ -0,0 +1,12 @@
+def main_menu():
+    print("Welcome to the .... choose from the following options:")
+    print("-Add student: 1")
+    print("-Update student info: 2")
+    print("-Remove a student: 3")
+
+    option = input()
+    return option
+
+def add_student():
+    pass
+
---
+++ main.py
@@ -0,0 +1,9 @@
+
+from functions import main_menu, add_student
+
+option = main_menu
+
+if(option == "1"):
+    add_student()
+elif option=="2":
+    pass
